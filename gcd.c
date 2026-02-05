6d0b060e8a6a8f662a09b567239b08244e9843d8
tanishadas066 <tanishadas014@gmail.com>
2026-02-05 15:34:55 +0530

    Create gcd.c

---
+++ gcd.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void main (){
+ int a.b.gcd:
+ printf("Enter two number: ");
+scanf("%d %d",&a,&b):
+int c=(ab)? a:b:
+
+for(int i=1;i<=c; i++){
+
+if(a% 10 && b % i == 0) {
+
+gcd-i:
+
+
+}
+
+ }
+
+printf("GCD-%d", gcd): 
+}
