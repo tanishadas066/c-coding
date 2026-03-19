079e8408f450233c14318e9e79c95479a1dd277c
tanishadas066 <tanishadas014@gmail.com>
2026-03-19 15:23:37 +0530

    Create Pattern.c

---
+++ Pattern.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+
+
+
+
+
+int main() {
+
+int i , j;
+
+
+
+
+
+for(i=0;i<=4;1++){
+if(i%2==0){
+
+
+int n=1;
+
+
+ for(j=0;j<i;j++){
+
+
+13 n+=2;
+
+printf("%d",n);
+
+
+
+}
+
+
+
+else
+
+
+
+int n=2;
+
+
+
+for(j=0;j<i;j++){
+
+
+
+
+
+printf("%d",n):
+
+
+
+n+=2;
+
+
+
+
+ printf("\n")
+
+}
+
+
+
+return 0:
+
+}
